d61f4287a9651031493f62bfe9efce92a4a8e13a
nilswende <s142075@student.dhbw-mannheim.de>
2015-10-28 12:15:35 +0100

    added Catapult class

---
+++ Quellcode/src/sample/Catapult.java
@@ -0,0 +1,36 @@
+/** @author: Nils Wende
+*/
+
+package sample;
+
+import javafx.scene.Line;
+import javafx.scene.Polygon;
+
+class Catapult
+{
+	Line ruler;
+	Polygon rubber;
+	Vector contactPoint;
+	
+	Catapult()
+	{
+		ruler = new Line(); //TODO fixed coordinates at start
+		rubber = new Polygon(); //TODO fixed position
+		contactPoint = new Vector(); //TODO fixed position at start
+	}
+	
+	void fire()
+	{
+		
+	}
+	
+	void moveLeft()
+	{
+		
+	}
+	
+	void moveRight()
+	{
+		
+	}
+}
