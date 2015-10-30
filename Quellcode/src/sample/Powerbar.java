78ab0933fc5f1e187803484a72b27daa0ddcd5d6
nilswende <s142075@student.dhbw-mannheim.de>
2015-10-30 20:34:34 +0100

    refactored

--- Quellcode/src/sample/Powerbar.java
+++ Quellcode/src/sample/Powerbar.java
@@ -1,39 +1,54 @@
-// Create by Simon on  30/10
-// Worked on by:
-// Simon
-public class Powerbar{
-	public static double MAX_POWER = 100.0;
+/**
+ * Created by Simon on 30/10
+ *
+ * @author Simon, Nils Wende
+ */
+
+public class Powerbar
+{
+	public static final double MAX_POWER = 100.0;
 	public double mPower;
 	public double mSpeed;
-	private double mTotalPower;
-	
-	public Powerbar(){
+
+	public Powerbar()
+	{
 		mPower = 0.0;
 		mSpeed = 0.0;
-		mTotalPower = 0.0;
 	}
-	public Powerbar(double power,double speed){
+
+	public Powerbar(double power, double speed)
+	{
 		mPower = power;
 		mSpeed = speed;
-		mTotalPower = power;
 	}
-	public void computePower(){
-		mTotalPower += mSpeed;
-		if(mTotalPower%(2*MAX_POWER) > MAX_POWER){
-			mPower = 2*MAX_POWER-mTotalPower%(2*MAX_POWER); 
+
+	public void computePower()
+	{
+		final double totalPower = mPower + mSpeed;
+		final double maxPowerDoubled = 2.0 * MAX_POWER;
+		final double normalizedPower = totalPower % maxPowerDoubled;
+		if (normalizedPower > MAX_POWER)
+		{
+			mPower = maxPowerDoubled - normalizedPower;
 		}
-		else{
-			mPower = mTotalPower%(2*MAX_POWER);
+		else
+		{
+			mPower = normalizedPower;
 		}
 	}
-	@Override 
-	public String toString(){
-		return "Powerbar ( Power " + mPower + " | Speed " + mSpeed + " | TotalPower " + mTotalPower + " )";
+
+	@Override
+	public String toString()
+	{
+		return "Powerbar ( Power " + mPower + " | Speed " + mSpeed + " )";
 	}
-	public static void main(String[] args){
-		Powerbar pb = new Powerbar(0.0,10.0);
+
+	public static void main(String[] args)
+	{
+		Powerbar pb = new Powerbar(0.0, 10.0);
 		int i = 0;
-		while(i<1000){
+		while (i < 1000)
+		{
 			System.out.println(pb);
 			pb.computePower();
 			++i;
