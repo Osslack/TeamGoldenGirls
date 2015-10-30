433381eed002e8aa2c9513c3f1f8dde56251bd5f
nilswende <s142075@student.dhbw-mannheim.de>
2015-10-30 17:34:46 +0100

    updated to new members of Vector2D

--- Quellcode/src/sample/Catapult.java
+++ Quellcode/src/sample/Catapult.java
@@ -1,45 +1,45 @@
-/** @author: Nils Wende
-*/
-
-package sample;
-
-import java.util.Observable;
+/**
+ * @author: Nils Wende
+ */
 
 import javafx.collections.ObservableList;
-
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
 
-/** assuming coordinate system with point of origin in lower left corner
-*/
+import java.util.Observable;
+
+/**
+ * assuming coordinate system with point of origin in lower left corner
+ */
 class Catapult extends Observable
 {
-	Polygon rubber;
-	Line ruler;
-	Vector pivotPoint;
+	Polygon  rubber;
+	Line     ruler;
+	Vector2D pivotPoint;
 
-	private final double rulerLength = 15.0; // in cm
-	private final double rubberSidelength = 3.0; // in cm
-	private final double rubberLeftVertexX = tipPositionX - rubberSidelength/2;
+	private final double rulerLength      = 15.0; // in cm
+	private final double rubberSideLength = 3.0; // in cm
+	private final double rubberLeftVertexX;
 	private final double minRulerOverhang = rulerLength * 0.1;
-	private final double step = 0.1; // in cm
+	private final double step             = 0.1; // in cm
 
 	Catapult()
 	{
 		final double paddingToLeftScreenEdge = 0.5;
 		final double tipPositionX = rulerLength - minRulerOverhang + paddingToLeftScreenEdge;
-		final double rubberHeight = Math.sqrt(3) * rubberSidelength / 2;
-		rubber = new Polygon(tipPositionX                        , rubberHeight,
-								tipPositionX + rubberSidelength/2, 0.0,
-								rubberLeftVertexX                , 0.0
-							); // equilateral triangle
+		final double rubberHeight = Math.sqrt(3) * rubberSideLength / 2;
+		rubberLeftVertexX = tipPositionX - rubberSideLength / 2;
+		rubber = new Polygon(tipPositionX, rubberHeight,
+							 tipPositionX + rubberSideLength / 2, 0.0,
+							 rubberLeftVertexX, 0.0
+		); // equilateral triangle
 
-		final double deltaX = Math.sqrt(rulerLength*rulerLength/4 - rubberHeight*rubberHeight);
+		final double deltaX = Math.sqrt(rulerLength * rulerLength / 4 - rubberHeight * rubberHeight);
 		final double xStart = tipPositionX - deltaX;
 		final double xEnd = tipPositionX + deltaX;
-		ruler = new Line(xStart, 0, xEnd, 2*rubberHeight); // tip of rubber is at half of ruler length, yStart is always 0
+		ruler = new Line(xStart, 0, xEnd, 2 * rubberHeight); // tip of rubber is at half of ruler length, yStart is always 0
 
-		pivotPoint = new Vector(tipPositionX, rubberHeight); // set to tip of rubber
+		pivotPoint = new Vector2D(tipPositionX, rubberHeight); // set to tip of rubber
 
 		this.setChanged();
 		this.notifyObservers();
@@ -52,7 +52,7 @@ class Catapult extends Observable
 
 	void moveRulerToLeft()
 	{
-		final Vector lineEnd = new Vector(ruler.getEndX(), ruler.getEndY());
+		final Vector2D lineEnd = new Vector2D(ruler.getEndX(), ruler.getEndY());
 		final double remainingLength = getDistanceBetween(pivotPoint, lineEnd);
 		if (remainingLength > minRulerOverhang)
 		{
@@ -67,26 +67,26 @@ class Catapult extends Observable
 		}
 	}
 
-	private double getDistanceBetween(Vector v, Vector w)
+	private double getDistanceBetween(Vector2D v, Vector2D w)
 	{
-		double deltaX = v.x - w.x;
-		double deltaY = v.y - w.y;
-		return Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+		double deltaX = v.mX - w.mX;
+		double deltaY = v.mY - w.mY;
+		return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 	}
 
 	private void setNewLineEndpoint()
 	{
-		final Vector lineStart = new Vector(ruler.getStartX(), 0);
-		final Vector newLineEnd = getPointOnLine(lineStart, pivotPoint, rulerLength);
-		ruler.setEndX(newLineEnd.x);
-		ruler.setEndY(newLineEnd.y);
+		final Vector2D lineStart = new Vector2D(ruler.getStartX(), 0);
+		final Vector2D newLineEnd = getPointOnLine(lineStart, pivotPoint, rulerLength);
+		ruler.setEndX(newLineEnd.mX);
+		ruler.setEndY(newLineEnd.mY);
 	}
 
-	private Vector getPointOnLine(Vector v, Vector w, double distanceFromV)
+	private Vector2D getPointOnLine(Vector2D v, Vector2D w, double distanceFromV)
 	{
 		final double length = getDistanceBetween(v, w);
-		double deltaX = v.x - w.x;
-		double deltaY = v.y - w.y;
+		double deltaX = v.mX - w.mX;
+		double deltaY = v.mY - w.mY;
 		// normalize the vectors
 		deltaX /= length;
 		deltaY /= length;
@@ -94,7 +94,7 @@ class Catapult extends Observable
 		deltaX *= distanceFromV;
 		deltaY *= distanceFromV;
 
-		return new Vector(v.x + deltaX, v.y + deltaY);
+		return new Vector2D(v.mX + deltaX, v.mY + deltaY);
 	}
 
 	void moveRulerToRight()
@@ -105,10 +105,10 @@ class Catapult extends Observable
 			// move the start point to the right
 			ruler.setStartX(startX + step);
 			// set the new end point
-			final Vector lineStart = new Vector(ruler.getStartX(), 0);
-			final Vector newLineEnd = getPointOnLine(lineStart, pivotPoint, rulerLength);
-			ruler.setEndX(newLineEnd.x);
-			ruler.setEndY(newLineEnd.y);
+			final Vector2D lineStart = new Vector2D(ruler.getStartX(), 0);
+			final Vector2D newLineEnd = getPointOnLine(lineStart, pivotPoint, rulerLength);
+			ruler.setEndX(newLineEnd.mX);
+			ruler.setEndY(newLineEnd.mY);
 
 			this.setChanged();
 			this.notifyObservers();
@@ -117,12 +117,12 @@ class Catapult extends Observable
 
 	void enlargeRubber()
 	{
-		 // increase the y-Coordinate of the rubber tip by the value of step
+		// increase the y-Coordinate of the rubber tip by the value of step
 		ObservableList<Double> points = rubber.getPoints();
 		final double yPos = points.get(1);
 		points.set(1, yPos + step);
 		// increase the y-Coordinate of the pivotPoint
-		pivotPoint.y += step;
+		pivotPoint.mY += step;
 		// move ruler
 		setNewLineEndpoint();
 
@@ -137,7 +137,7 @@ class Catapult extends Observable
 		final double yPos = points.get(1);
 		points.set(1, yPos - step);
 		// decrease the y-Coordinate of the pivotPoint
-		pivotPoint.y -= step;
+		pivotPoint.mY -= step;
 		// move ruler
 		setNewLineEndpoint();
 
@@ -148,7 +148,7 @@ class Catapult extends Observable
 
 	/* method overloads: */
 
-	private Vector getPointOnLine(double x1, double y1, double x2, double y2, double distanceFrom1)
+	private Vector2D getPointOnLine(double x1, double y1, double x2, double y2, double distanceFrom1)
 	{
 		final double length = getDistanceBetween(x1, y1, x2, y2);
 		double deltaX = x1 - x2;
@@ -160,13 +160,13 @@ class Catapult extends Observable
 		deltaX *= distanceFrom1;
 		deltaY *= distanceFrom1;
 
-		return new Vector(x1 + deltaX, y1 + deltaY);
+		return new Vector2D(x1 + deltaX, y1 + deltaY);
 	}
 
 	private double getDistanceBetween(double x1, double y1, double x2, double y2)
 	{
 		double deltaX = x1 - x2;
 		double deltaY = y1 - y2;
-		return Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+		return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 	}
 }
